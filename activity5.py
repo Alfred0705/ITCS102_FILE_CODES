95efb741e1b864aaa9f0f8fb5aac0374ec7416b3
Alfred0705 <tideraalfred0@gmail.com>
2026-08-26 11:05:51 +0800

    python activity 5

---
+++ activity5.py
@@ -0,0 +1,18 @@
+#operators activity
+
+a = 7
+b = 5
+sum = a + b
+diff = a - b
+times = a * b
+modulus = a % b
+exponets = a ** b
+floor_division = a // b
+
+
+print("The sum of ",a," and",b," is",sum)
+print("The difference of ",a," minus ",b," is",diff)
+print("The percent of ",a," modulus ",b," is",modulus)
+print("The product of ",a," times ",b," is",times)
+print("The exponet of ",a," and ",b," is",exponets)
+print("The floor division of ",a," and ",b," is",floor_division)
