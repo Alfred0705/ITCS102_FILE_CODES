3b73377f6f2a7e5ddd4e96c9ddd0268afe654a87
Alfred0705 <tideraalfred0@gmail.com>
2026-09-06 22:59:56 +0800

    python activity7

---
+++ activity7.py
@@ -0,0 +1,66 @@
+#assignment operation ( =, += , -= , *= , /*)
+
+
+
+total = 29 
+
+n = eval(input("Enter a number --> "))
+
+total += n 
+
+print("current total is--->",total)
+
+x = eval(input("Enter a number --> "))
+
+total *= x 
+
+print("current total is--->",total)
+
+a = eval(input("Enter a number --> "))
+
+total /= a
+
+print("current total is--->",total)
+
+m = eval(input("Enter a number --> "))
+
+total -= m
+
+print("current total is--->",total)
+
+t = eval(input("Enter a number --> "))
+
+total *= t
+
+print("current total is--->",total)
+
+u = eval(input("Enter a number --> "))
+
+total += u 
+
+print("current total is--->",total)
+
+z = eval(input("Enter a number --> "))
+
+total -= z
+
+print("current total is--->",total)
+
+p = eval(input("Enter a number --> "))
+
+total = p 
+
+print("current total is--->",total)
+
+o = eval(input("Enter a number --> "))
+
+total += o
+
+print("current total is--->",total)
+
+h = eval(input("Enter a number --> "))
+
+total *= h
+
+print("current total is--->",total)
+
