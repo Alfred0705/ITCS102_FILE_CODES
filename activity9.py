c19fda03d19c0b713e7f3658d215a9b8a9d7e86c
Alfred0705 <tideraalfred0@gmail.com>
2026-09-07 10:48:14 +0800

    python activity9

---
+++ activity9.py
@@ -0,0 +1,32 @@
+#relational operator activity 
+
+x = 10 
+y = 5 
+
+password1 = "alfred"
+password2 = "alfred"
+
+#1
+print(x > y) 
+#2
+print(x < y) 
+#3
+print(x >= y)
+#4
+print(x <= y) 
+#5
+print(x != y) 
+#6
+print(x == y) 
+#7
+print(password1 == password2)
+#8
+print(password1 != password2)
+#9
+print(password1 > password2)
+#10
+print(password1 < password2)
+#11
+print(password1 <= password2)
+#12
+print(password1 >= password2)
