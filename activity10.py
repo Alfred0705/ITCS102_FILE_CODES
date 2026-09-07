666d0982b994d2c4fc2b96dc26ca8759e9a35934
Alfred0705 <tideraalfred0@gmail.com>
2026-09-07 11:15:10 +0800

    python activity 10

---
+++ activity10.py
@@ -0,0 +1,16 @@
+#basic if else program
+
+username = "user1" 
+password = "number_1"
+
+u = input("Input USERNAME---> ") 
+p = input("Input PASSWORD---> ")
+
+if u == username and p == password:
+	print("username and password correct")
+
+else: 
+	print("access denied")
+
+
+
