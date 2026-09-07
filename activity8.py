2dc95863341d6bd810b50be8df2bf58b1e978a06
Alfred0705 <tideraalfred0@gmail.com>
2026-09-07 10:29:56 +0800

    activity8.py

---
+++ activity8.py
@@ -0,0 +1,36 @@
+#assignment operation
+
+hobbies = "," #empty string
+
+h = input("What are you hobbies--->")
+hobbies += h + ", " 
+
+x = input("What are you hobbies--->")
+hobbies += x + ", "
+
+i = input("What are you hobbies--->")
+hobbies += i + ", "
+
+o = input("What are you hobbies--->")
+hobbies += o + ", "
+
+p = input("What are you hobbies--->")
+hobbies += p + ", "
+
+h = input("What are you hobbies--->")
+hobbies += h + ", "
+
+h = input("What are you hobbies--->")
+hobbies += h + ", "
+
+h = input("What are you hobbies--->")
+hobbies += h + ", "
+ 
+h = input("What are you hobbies--->")
+hobbies += h + ", "
+
+h = input("What are you hobbies--->")
+hobbies += h + ", "
+
+
+print("My hobbies are{",hobbies,"}")
