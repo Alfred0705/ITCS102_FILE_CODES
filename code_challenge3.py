fb678b28e5a1219e68779b301035d90fef794d44
Alfred0705 <tideraalfred0@gmail.com>
2026-09-09 13:10:23 +0800

    baseline new

--- code_challenge3.py
+++ code_challenge3.py
@@ -1,10 +1,29 @@
 #Global Freight Calculator
 
-name = input("Sender name ---> ")
+name = input("sender name ---> ")
+wieght = float(input("Enter weight(kg): "))
+distance = float(input("Enter distance(km) "))
+is_express = input("is it express?(True/False): ") == True
+is_international = input("is it international?(True/False): ") == True
 
-#type of item 
 
-isFragile == true
-print("the item is fragile",isFragile)
+#calculate
 
+base_cost = (weight * 2.50) + (distance * 0.15) 
 
+#evaluate pricing tiers
+
+if weight <= 2.0 and distance <= 100 and not is_express and not_international:
+	total = 0.00
+
+elif is_international and is_express:
+	total = (base_cost * 1.40) + 50
+
+elif (is_express or is_international) and weight >20:
+	total = (base_cost * 1.20) + 25
+
+elif weight >30 or distance >1000: 
+	total = base_cost + 30
+
+else: 
+	total = base_cost
