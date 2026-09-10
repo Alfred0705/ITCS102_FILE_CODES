7ffdcb0ab815389d1bf51d0bf8200308026d27f8
Alfred0705 <tideraalfred0@gmail.com>
2026-09-10 08:35:24 +0800

    code_challenge3

--- code_challenge3.py
+++ code_challenge3.py
@@ -1,29 +1,38 @@
-#Global Freight Calculator
+# Global Freight Calculator
 
-name = input("sender name ---> ")
-wieght = float(input("Enter weight(kg): "))
-distance = float(input("Enter distance(km) "))
-is_express = input("is it express?(True/False): ") == True
-is_international = input("is it international?(True/False): ") == True
+name = input("Sender name: ")
+weight = float(input("Enter weight (kg): "))
+distance = float(input("Enter distance (km): "))
+is_express = input("Is it express? (y/n): ").lower() == "y"
+is_international = input("Is it international? (y/n): ").lower() == "y"
+is_rush = input("Is the item rush? (y/n): ").lower() == "y"
 
+# Calculate
+base_cost = (weight * 2.50) + (distance * 0.15)
 
-#calculate
-
-base_cost = (weight * 2.50) + (distance * 0.15) 
-
-#evaluate pricing tiers
-
-if weight <= 2.0 and distance <= 100 and not is_express and not_international:
-	total = 0.00
+# Evaluate pricing tiers
+if weight <= 2.0 and distance <= 100 and not is_express and not is_international:
+    total = 0.00
 
 elif is_international and is_express:
-	total = (base_cost * 1.40) + 50
+    total = (base_cost * 1.40) + 50
 
-elif (is_express or is_international) and weight >20:
-	total = (base_cost * 1.20) + 25
+elif (is_express or is_international) and weight > 20:
+    total = (base_cost * 1.20) + 25
 
-elif weight >30 or distance >1000: 
-	total = base_cost + 30
+elif weight > 30 or distance > 1000:
+    total = base_cost + 30
 
-else: 
-	total = base_cost
+elif is_rush:
+    total = base_cost + 20
+
+else:
+    total = base_cost
+
+
+# Display result
+
+print("==================================")
+
+print(f"Total shipping charge: ${total:.2f}")
+
